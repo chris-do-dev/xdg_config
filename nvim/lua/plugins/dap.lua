3a4ed99bede5c67c7f335bba1b45ed3aaa5bfcb2
Chris Wilson <hey@chris-wilson.me>
2024-09-20 16:38:45 +0100

    Add keymaps for dap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -14,8 +14,21 @@ return {
       dapgo.setup()
       dapui.setup()
 
+      vim.keymap.set("n", "<F1>", dap.continue, {})
+      vim.keymap.set("n", "<F2>", dap.step_over, {})
+      vim.keymap.set("n", "<F3>", dap.step_into, {})
+      vim.keymap.set("n", "<F4>", dap.step_out, {})
+      vim.keymap.set("n", "<F5>", dap.step_back, {})
+      vim.keymap.set("n", "<F13>", dap.restart, {})
+
       vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
-      vim.keymap.set("n", "<leader>dc", dap.continue, {})
+
+      vim.keymap.set("n", "<leader>dt", dapgo.debug_test, {})
+      vim.keymap.set("n", "<leader>dl", dapgo.debug_last_test, {})
+
+      vim.keymap.set("n", "<space>?", function()
+        dapui.eval(nil, { enter = true })
+      end)
 
       dap.listeners.before.attach.dapui_config = function()
         dapui.open()
