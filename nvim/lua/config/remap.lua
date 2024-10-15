0dd80d3b8c7f3b2630e81a13c824955ecef59955
Chris Wilson <hey@chris-wilson.me>
2024-10-15 21:48:53 +0100

    Highlight when yanking text in nvim

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -5,3 +5,14 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Visual mode keymaps
 vim.keymap.set("v", "<leader>y", '"*y')
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
