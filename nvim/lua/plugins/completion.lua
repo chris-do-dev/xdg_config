f639a9030cd8439de4ebc2346f27bcc830a91505
Chris Wilson <hey@chris-wilson.me>
2024-10-15 21:49:19 +0100

    Remove snippets from nvim

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -2,17 +2,12 @@ return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
-      "hrsh7th/cmp-path" ,
-      "hrsh7th/cmp-nvim-lsp" ,
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
       local cmp = require("cmp")
       local cmp_select = { behaviour = cmp.SelectBehavior.Select }
-
-      require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
         window = {
@@ -30,17 +25,10 @@ return {
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "path" },
-          { name = "luasnip" },
         }, {
           { name = "buffer" },
         }),
-
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
       })
-    end
-  }
+    end,
+  },
 }
