ea8208e328ce164d6e2eee4650d2d010bd90a908
Chris Wilson <hey@chris-wilson.me>
2024-11-06 01:15:25 +0000

    Fix formatting

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,50 +1,54 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		dependencies = {
-			"williamboman/mason.nvim",
-		},
-		keys = {
-			{
-				"<leader>f",
-				function()
-					require("conform").format({ async = true })
-				end,
-				mode = "",
-				desc = "Format buffer",
-			},
-		},
-		init = function()
-			require("conform").setup({
-				format_on_save = {
-					timeout_ms = 500,
-					lsp_format = "fallback",
-				},
-				formatters_by_ft = {
-					["*"] = {
-						"trim_whitespace",
-						"trim_newlines",
-					},
-					lua = { "stylua" },
-					javascript = {
-						"prettier",
-					},
-					typescript = {
-						"prettier",
-					},
-					go = {
-						"goimports",
-						"goimports-reviser",
-						"gofumpt",
-						"golines",
-					},
-					terraform = { "terraform_fmt" },
-					sh = { "shellcheck" },
-					zsh = { "shellcheck" },
-				},
-			})
-		end,
-	},
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    keys = {
+      {
+        "<leader>f",
+        function()
+          require("conform").format({ async = true })
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+    init = function()
+      require("conform").setup({
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_format = "fallback",
+        },
+        formatters_by_ft = {
+          ["*"] = {
+            "trim_whitespace",
+            "trim_newlines",
+          },
+          lua = { "stylua" },
+          javascript = {
+            "prettier",
+          },
+          typescript = {
+            "prettier",
+          },
+          markdown = {
+            "mdformat",
+            args = { "--wrap 80" },
+          },
+          go = {
+            "goimports",
+            "goimports-reviser",
+            "gofumpt",
+            "golines",
+          },
+          terraform = { "terraform_fmt" },
+          sh = { "shellcheck" },
+          zsh = { "shellcheck" },
+        },
+      })
+    end,
+  },
 }
