1e7e2ec35d3fb8b8e2aa2abd25ba044f8cd0efa4
Chris Wilson <cwilson@netboxlabs.com>
2025-07-15 15:25:21 +0100

    Fix nvm config in zsh

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,6 +1,6 @@
 export NVM_DIR="${HOME}/.nvm"
-[ -s "${NVM_DIR}/nvm.sh" ] && source "${NVM_DIR}/nvm.sh"
-[ -s "${NVM_DIR}/bash_completion" ] && source "${NVM_DIR}/bash_completion"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && source "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && source "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 autoload -U add-zsh-hook
 load-nvmrc() {
