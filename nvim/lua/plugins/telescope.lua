81733a3c8b21a584e2787a8ae7379586e1a1af96
Chris Wilson <cwilson@netboxlabs.com>
2025-08-04 17:19:02 +0100

    Search hidden files with telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,16 +7,12 @@ return {
 		local builtin = require("telescope.builtin")
 
 		telescope.setup({
-			--		extensions = {
-			--			fzf = {
-			--				fizzy = true,
-			--				override_generic_sorter = true,
-			--				override_file_sorter = true,
-			--				case_mode = "smart_case",
-			--			},
-			--		},
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
 		})
-		--telescope.load_extension("fzf")
 
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
