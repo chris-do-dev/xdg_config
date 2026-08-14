7b1f0aa2161cffc4a35e0cb7b8e9ce2f3da1ba2e
Chris Wilson <hey@chris-wilson.me>
2026-08-14 12:51:19 +0100

    [NVIM] Add fugitive keymaps

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -3,6 +3,11 @@ return {
 		"tpope/vim-fugitive",
 		config = function()
 			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+			vim.keymap.set("n", "<leader>gb", function()
+				vim.cmd.Git("blame")
+			end)
+			vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiffsplit)
+			vim.keymap.set("n", "<leader>gl", vim.cmd.Gclog)
 		end,
 	},
 }
